851c049109376cb859bdafb3b72c793133ccc9c6
kdpark <kdpark@kw.ac.kr>
2024-05-20 18:04:19 +0900

    CORS, delete added

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from todo import todo_router # added
 import uvicorn
 
 app = FastAPI()
+
+origins = ["http://127.0.0.1:5500", "http://50.19.226.105"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 @app.get("/")
 async def welcome() -> dict:
